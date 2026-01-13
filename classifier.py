e7602c355bea1a67bc8bf1c22e807db961aa44a1
Ardelia Luthfiyah <ardelialuthfiyah@Ardelias-MacBook-Air.local>
2026-01-13 15:53:55 +0100

    Update: classifier.py

--- classifier.py
+++ classifier.py
@@ -7,8 +7,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import Pipeline
 
-# paths inside your project
-TRAIN_JSON_PATH = "label_tr.json"            # copy this from Drive into your repo
+TRAIN_JSON_PATH = "label_tr.json"            
 STOPWORDS_CSV_PATH = "combined_stopwords.csv"
 
 TARGET_USERS = [
